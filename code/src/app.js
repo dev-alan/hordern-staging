2059f13b826059c3187bd8fe92d67d471626e6a7
Alan <alanl@sitesuite.com.au>
2017-10-12 16:28:15 +1100

    adding webpack

---
+++ code/src/app.js
@@ -0,0 +1,1 @@
+// require("!style-loader!less-loader!./styles.less");
--- code/webpack.config.js
+++ code/webpack.config.js
@@ -1,18 +1,32 @@
+const path = require('path');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-  entry: ["./app.js"],
+  entry: './src/app.js',
   output: {
-    filename: "./dist/assets/bundle.min.js"
+    path: path.resolve(__dirname, 'dist/assets/'),
+    filename: 'bundle.min.js'
   },
   module: {
     loaders: [
-      {test: /\.scss$/i, loader: extractCSS.extract(['css','sass'])},
-      {test: /\.less$/i, loader: extractLESS.extract(['css','less'])}
+      {
+        test: /\.css$/,
+        loader: 'style!css'
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: "style-loader" // creates style nodes from JS strings
+          },
+          {
+            loader: "css-loader" // translates CSS into CommonJS
+          },
+          {
+            loader: "less-loader" // compiles Less to CSS
+          }
+        ]
+      }
     ]
-  },
-  plugins: [
-    new ExtractTextPlugin("styles.css"),
-  ],
-  watch: true
-}
+  }
+};
